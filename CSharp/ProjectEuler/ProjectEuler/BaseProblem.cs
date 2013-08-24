4aab040fb274b33b09481d1c5674d887204ecf8d
Bret Koppel <git@bretkoppel.com>
2013-08-24 14:06:17 -0400

    Make benchmarks more consistent.

--- CSharp/ProjectEuler/ProjectEuler/BaseProblem.cs
+++ CSharp/ProjectEuler/ProjectEuler/BaseProblem.cs
@@ -12,21 +12,32 @@ namespace ProjectEuler
         public void Benchmarks()
         {
             var methods = this.GetType().GetMethods();
-            foreach (var methodInfo in methods.Where(methodInfo => methodInfo.Name != "Benchmarks" && Attribute.GetCustomAttribute((MemberInfo) methodInfo, typeof(TestAttribute)) != null))
+            var testMethods = methods.Where(methodInfo => methodInfo.Name != "Benchmarks" && Attribute.GetCustomAttribute((MemberInfo) methodInfo, typeof(TestAttribute)) != null);
+            if (testMethods.Any() == false)
+                return;
+
+            foreach (var methodInfo in testMethods)
             {
-                Benchmark((Action)Delegate.CreateDelegate(typeof(Action), this, methodInfo.Name));
+                var elapsed = Benchmark((Action)Delegate.CreateDelegate(typeof(Action), this, methodInfo.Name));
+                Debug.WriteLine(methodInfo.Name + ": " + elapsed + "ms");
             }
         }
 
-        private void Benchmark(Action action)
+        private long Benchmark(Action action)
         {
+            // warm up the benchmark
+            action();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
             var watch = new Stopwatch();
             watch.Restart();
-            for (int i = 0; i < 500; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 action();
             }
-            Debug.WriteLine(action.Method.Name + ":" + watch.ElapsedMilliseconds);
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
         }
     }
 }
