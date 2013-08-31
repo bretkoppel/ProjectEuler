22f6b1148d3e0d2d4e2fdeeefa785b45b8540802
Bret Koppel <git@bretkoppel.com>
2013-08-30 20:24:27 -0400

    Return frequency of factors in Clojure sln to problem 3.

--- Clojure/problem3.clj
+++ Clojure/problem3.clj
@@ -1,11 +1,14 @@
 (ns clojure-problem3
 	(:require [clojure.test :as test]))
 
-(defn prime-factors-by-trial-division [value]	
+(defn prime-factors-by-trial-division
+	"Gets the frequencies of each prime factor for value."
+	[value]	
 	(loop [v value testers (range 3 value 2) primes []]
 		(let [t (first testers)]
 			(cond
-				(.isProbablePrime (BigInteger. (str v)) 3) (set (conj primes v))
+				(< v 3) (frequencies (conj primes v))
+				(.isProbablePrime (BigInteger. (str v)) 3) (frequencies (conj primes v))
 				(zero? (mod v 2)) (recur (/ v 2) testers (conj primes 2))
 				(and (.isProbablePrime (BigInteger. (str t)) 3) (zero? (mod v t))) (recur (/ v t) testers (conj primes t))
 				:else (recur v (next testers) primes)
@@ -13,5 +16,6 @@
 		)
 	)
 )
-(println (first (sort-by - (prime-factors-by-trial-division 600851475143))))
-(test/is (=  (first (sort-by - (prime-factors-by-trial-division 600851475143))) 6857) "Solution 1")
+(println "Factor Frequencies: " (prime-factors-by-trial-division 600851475143))
+(println "Max Factor: " (first (sort-by - (keys (prime-factors-by-trial-division 600851475143)))))
+(test/is (=  (first (sort-by - (keys (prime-factors-by-trial-division 600851475143)))) 6857) "Solution 1")
